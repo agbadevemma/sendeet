0fd275279f792f5a43701953553527bd67914af3
himarnoel <olaimarnoel@gmail.com>
2024-10-20 14:49:29 +0100

    working

--- src/components/buttons/ButtonPrimary.tsx
+++ src/components/buttons/ButtonPrimary.tsx
@@ -3,10 +3,17 @@ import React from "react";
 
 type Props = {
   size?: "sm" | "lg";
-  type?: "";
-  disabled?:false;
+  type?:
+    | "primary"
+    | "secondary"
+    | "tertiary"
+    | "text"
+    | "greys"
+    | "destructive";
+
+  disabled?: boolean;
   state?: "default" | "hover" | "focused" | "disabled";
-  icon_style?: "leading-icon" | "trailing icon" | "icon-only";
+  icon_style?: "" | "leading-icon" | "trailing icon" | "icon-only";
   text?: string;
   onClick?: () => void;
   iconSrc?: string;
@@ -16,10 +23,12 @@ type Props = {
 const Button = ({
   text = "",
   onClick,
-  disabled,
+  size = "sm",
+  disabled = false,
+  icon_style = "", //when there is no icon style and icon at all
   iconSrc,
   className,
-  type 
+  type = "secondary",
 }: Props) => {
   return (
     <div>
@@ -27,10 +36,40 @@ const Button = ({
       <button
         onClick={onClick}
         disabled={disabled}
-        className={`font-semibold text-md py-[8px] px-[12px] lg:px-[24px] lg:py-[16px] w-full  rounded-lg shadow-[0px_1px_2px_0px_rgba(16,_24,_40,_0.24),_0px_0px_0px_1px_#00AAF7]  bg-[#00AAF7] bg-gradient-to-b flex items-center justify-center gap-2 from-white/[0.2] to-[#0079AF33]/[0.2]   text-white ${className}`}
+        className={`font-semibold 
+           ${size == "sm" && "text-sm font-medium  py-[8px] px-[16px]"}
+            ${size == "lg" && "text-md font-medium  py-[16px] px-[24px]"}
+             ${
+               size == "sm" && icon_style === "icon-only" ? "text-sm  p-2 " : ""
+             }
+             ${
+               size == "lg" && icon_style === "icon-only" ? "text-sm  p-4 " : ""
+             }
+              ${
+                size == "sm" &&
+                (icon_style === "leading-icon" || "trailing icon")
+                  ? "text-sm font-semibold py-[8px] px-[12px]"
+                  : ""
+              }
+              ${
+                size == "lg" &&
+                (icon_style === "leading-icon" || "trailing icon")
+                  ? "text-sm font-semibold py-[16px]"
+                  : ""
+              }
+           ${
+             type === "primary" &&
+             " hover:bg-primary-600 bg-primary-500  hover:from-transparent hover:to-transparent  text-white from-white/[0.2] to-[#0079AF33]/[0.2]  bg-gradient-to-b focus:bg-primary-700 hover:shadow-transparent focus:shadow-transparent disabled:bg-primary-100 disabled:from-transparent disabled:to-transparent  shadow-[0px_1px_2px_0px_rgba(16,_24,_40,_0.24),_0px_0px_0px_1px_#00AAF7] disabled:shadow-transparent"
+           }
+
+            ${
+              type === "secondary" &&
+              "s hover:bg-grey-50 cart text-grey-800 hover:from-transparent hover:to-transparent  border border-white border-solid  bg-gradient-to-b from-white to-white/60 hover:shadow-none shadow-[0px_1px_2px_0px_rgba(16,_24,_40,_0.10),_0px_0px_0px_1px_rgba(185,_189,_199,_0.20)] focus:bg-primary-700 hover:shadow-transparent focus:shadow-transparent disabled:bg-primary-100 disabled:from-transparent disabled:to-transparent   disabled:shadow-transparent"
+            }
+           w-full  rounded-lg flex items-center justify-center gap-2    ${className}`}
         aria-label={text}
       >
-        {text}sdsd
+        {text}dfsadfa
         {iconSrc && (
           <span className="mr-2">
             <Image alt="icon" src={iconSrc} />
