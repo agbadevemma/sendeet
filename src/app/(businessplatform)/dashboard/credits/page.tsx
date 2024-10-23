f8a1a80dd40aa1b8ccbbde49378f30394a9b64f6
himarnoel <olaimarnoel@gmail.com>
2024-10-23 17:22:14 +0100

    finished working on credits table

--- src/app/(businessplatform)/dashboard/credits/page.tsx
+++ src/app/(businessplatform)/dashboard/credits/page.tsx
@@ -1,5 +1,7 @@
 import Button from "@/components/buttons/Button";
+import ArrowUp from "@/icons/arrow-up";
 import Calendar from "@/icons/calendar";
+import DotV from "@/icons/dot-v";
 import FileDownload from "@/icons/file-download";
 import FilterAlt from "@/icons/filter-alt";
 import IconStack from "@/icons/icon-stack";
@@ -10,9 +12,43 @@ import React from "react";
 type Props = {};
 
 const Credits = (props: Props) => {
+  interface Transaction {
+    code: string;
+    date: string;
+    creditPurchased: string;
+    description: string;
+    creditUsed: string;
+    status: "successful" | "pending" | "failed";
+  }
+  const transactions: Transaction[] = [
+    {
+      code: "TXN12345",
+      date: "10/10/24",
+      creditPurchased: "500",
+      description: "Purchased 500 credits",
+      creditUsed: "-",
+      status: "successful",
+    },
+    {
+      code: "TXN12346",
+      date: "10/11/24",
+      creditPurchased: "1000",
+      description: "Purchased 1000 credits",
+      creditUsed: "-",
+      status: "successful",
+    },
+    {
+      code: "TXN12347",
+      date: "10/12/24",
+      creditPurchased: "200",
+      description: "Purchased 200 credits",
+      creditUsed: "50",
+      status: "pending",
+    },
+  ];
   return (
     <div>
-      <div className="flex items-center justify-between">
+      <div className="flex flex-col lg:flex-row items-start gap-4 lg:items-center justify-between">
         <div className="flex items-center gap-4">
           <div className=" flex items-center justify-center p-4 shadow-[0px_1px_1px_0px_rgba(16,_24,_40,_0.10)] rounded-lg border border-grey-50">
             <Money1 color="black" />
@@ -54,7 +90,7 @@ const Credits = (props: Props) => {
       </div>
 
       <div className="rounded-xl mt-7  h-full w-full border border-[#E4E7EC] ">
-        <div className="flex gap-4 px-6 py-4  items-center  justify-between">
+        <div className="flex flex-col lg:flex-row lg:gap-4 px-6 py-4 gap-8 lg:items-center  justify-between">
           <div className="flex  flex-col gap-1">
             <p className=" text-[18px] font-medium text-[#101828]">
               Credit Purchase History
@@ -79,14 +115,82 @@ const Credits = (props: Props) => {
           </div>
         </div>
       </div>
-      <div className="flex w-full mx-auto px-6">
-        <table className="bg-[#F9FAFB]">
-         <thead className="text-grey-600">
-          <tr>
-            <th className="flex items-center">Transaction code</th>
-            <th className="flex items-center">Date</th>
-          </tr>
-         </thead>
+      <div className="flex w-full mx-auto px-6 overflow-auto mt-[18px]">
+        <table className=" w-full">
+          <thead className="text-grey-600 rounded">
+            <tr className="bg-[#F9FAFB]">
+              <th className=" pl-6 pr-2 py-2  rounded-s-lg">
+                <div className="flex items-center text-nowrap gap-2 w-full">
+                  Transaction code <ArrowUp color="#5D6679" />
+                </div>
+              </th>
+              <th className=" p-2">
+                <div className="flex items-center text-nowrap gap-2 w-full">
+                  Date
+                  <ArrowUp color="#5D6679" />
+                </div>
+              </th>
+              <th className="p-2">
+                <div className="flex items-center text-nowrap gap-2">
+                  Credit purchased <ArrowUp color="#5D6679" />
+                </div>
+              </th>
+              <th className="p-2">
+                <div className="flex items-center text-nowrap gap-2">
+                  Description <ArrowUp color="#5D6679" />
+                </div>
+              </th>
+              <th className="p-2">
+                <div className="flex items-center text-nowrap gap-2">
+                  Credit used <ArrowUp color="#5D6679" />
+                </div>
+              </th>
+              <th className="p-2 rounded-e-lg">
+                <div className="flex items-center text-nowrap">Status</div>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {transactions.map((transaction, index) => (
+              <tr
+                key={transaction.code}
+                className="border-b border-b-grey-50 hover:bg-gray-50"
+              >
+                <td className="font-medium text-grey-800 p-4 pl-6">
+                  {transaction.code}
+                </td>
+                <td className="font-medium text-grey-800 p-2 pr-8">
+                  {transaction.date}
+                </td>
+                <td className="font-medium text-grey-800 p-2">
+                  {transaction.creditPurchased}
+                </td>
+                <td className="font-medium text-grey-800 p-2">
+                  {transaction.description}
+                </td>
+                <td className="font-medium text-grey-800 p-2">
+                  {transaction.creditUsed}
+                </td>
+                <td className="font-medium text-grey-800 p-2">
+                  <div className="flex gap-x-2">
+                    <p
+                      className={`flex items-center  min-h-[24px] w-[94px] justify-center font-medium py-[2px] text-sm px-[10px] rounded-2xl
+                  ${
+                    transaction.status === "successful"
+                      ? "bg-success-50 text-success-800"
+                      : "bg-yellow-50 text-yellow-800"
+                  }`}
+                    >
+                      {transaction.status}
+                    </p>
+                    <div className="h-8 w-8 p-2 flex items-center rounded-lg border border-[#E4E7EC]">
+                      <DotV color="#101928" />
+                    </div>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     </div>
