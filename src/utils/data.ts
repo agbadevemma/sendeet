8495cfdde09fa45c8fdaa4908320d427bbc9896e
himarnoel <olaimarnoel@gmail.com>
2024-10-24 17:02:40 +0100

    dummy data for transaction added

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -40,3 +40,112 @@ export const subscriberRangeOptions: Option[] = [
   { value: "10001-100000", label: "10,001-100,000 subscribers" },
   { value: "100001+", label: "100,001+ subscribers" },
 ];
+
+export interface Transaction {
+  code: string;
+  date: string;
+  creditPurchased: string;
+  description: string;
+  creditUsed: string;
+  status: "successful" | "pending" | "failed";
+}
+
+// export const initialTransactions: Transaction[] = [
+//   {
+//     code: "TXN12345",
+//     date: "10/10/24",
+//     creditPurchased: "500",
+//     description: "Purchased 500 credits",
+//     creditUsed: "-",
+//     status: "successful",
+//   },
+//   {
+//     code: "TXN12346",
+//     date: "10/11/24",
+//     creditPurchased: "1000",
+//     description: "Purchased 1000 credits",
+//     creditUsed: "-",
+//     status: "successful",
+//   },
+//   {
+//     code: "TXN12347",
+//     date: "10/12/24",
+//     creditPurchased: "200",
+//     description: "Purchased 200 credits",
+//     creditUsed: "50",
+//     status: "failed",
+//   },
+//   {
+//     code: "TXN12348",
+//     date: "10/13/24",
+//     creditPurchased: "300",
+//     description: "Purchased 300 credits",
+//     creditUsed: "-",
+//     status: "pending",
+//   },
+//   {
+//     code: "TXN12349",
+//     date: "10/14/24",
+//     creditPurchased: "750",
+//     description: "Purchased 750 credits",
+//     creditUsed: "100",
+//     status: "successful",
+//   },
+//   {
+//     code: "TXN12350",
+//     date: "10/15/24",
+//     creditPurchased: "500",
+//     description: "Purchased 500 credits",
+//     creditUsed: "-",
+//     status: "pending",
+//   },
+//   {
+//     code: "TXN12351",
+//     date: "10/16/24",
+//     creditPurchased: "250",
+//     description: "Purchased 250 credits",
+//     creditUsed: "50",
+//     status: "failed",
+//   },
+//   {
+//     code: "TXN12352",
+//     date: "10/17/24",
+//     creditPurchased: "1000",
+//     description: "Purchased 1000 credits",
+//     creditUsed: "200",
+//     status: "successful",
+//   },
+//   {
+//     code: "TXN12353",
+//     date: "10/18/24",
+//     creditPurchased: "500",
+//     description: "Purchased 500 credits",
+//     creditUsed: "-",
+//     status: "pending",
+//   },
+//   {
+//     code: "TXN12354",
+//     date: "10/19/24",
+//     creditPurchased: "600",
+//     description: "Purchased 600 credits",
+//     creditUsed: "-",
+//     status: "successful",
+//   },
+//   {
+//     code: "TXN12355",
+//     date: "10/20/24",
+//     creditPurchased: "1200",
+//     description: "Purchased 1200 credits",
+//     creditUsed: "300",
+//     status: "failed",
+//   },
+  
+  
+ 
+// ];
+
+
+export const initialTransactions: Transaction[] = [
+  
+ 
+];
