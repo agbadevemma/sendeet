26b8076a9cd8fdbee45d1a25469f3251a0d1db01
himarnoel <olaimarnoel@gmail.com>
2024-11-01 03:38:55 +0100

    returned back to default

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,12 +17,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-
 }: Readonly<{
   children: React.ReactNode;
-  types: {
-    page: React.ReactNode
-  }
 }>) {
   return (
     <html lang={`en`}>
