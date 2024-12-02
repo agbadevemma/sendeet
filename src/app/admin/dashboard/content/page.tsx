59527df4a94d0b7a190795573d34ffdfe333f03c
himarnoel <olaimarnoel@gmail.com>
2024-12-02 02:47:27 +0100

    content

--- src/app/admin/dashboard/content/page.tsx
+++ src/app/admin/dashboard/content/page.tsx
@@ -53,7 +53,10 @@ const Content = (props: Props) => {
     );
   };
   const [filterTabs, setFilterTabs] = useState<Array<string>>([
-    "All", "Active", "Completed", "Drafts",
+    "All",
+    "Active",
+    "Completed",
+    "Drafts",
   ]);
 
   const [activeFilter, setActiveFilter] = useState<string>("All");
@@ -100,8 +103,8 @@ const Content = (props: Props) => {
       .filter((campaign) =>
         campaign.name.toLowerCase().includes(searchQuery.toLowerCase())
       )
-      .filter((campaign) =>
-        activeFilter === "All" || campaign.status === activeFilter
+      .filter(
+        (campaign) => activeFilter === "All" || campaign.status === activeFilter
       );
   };
 
@@ -131,7 +134,6 @@ const Content = (props: Props) => {
     selectedItems.length > 0 && selectedItems.length < mockCampaignData.length;
   return (
     <div>
-      
       <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-4 pb-10 mt-10">
         {/* Insight Cards */}
         <Card
@@ -203,198 +205,200 @@ const Content = (props: Props) => {
             />
           </div>
         </div>
-        <div className="flex flex-col w-full mx-auto overflow-auto mt-[18px] max-h-[814px]">
-          <table className="w-full">
-            <thead className="text-grey-600 rounded sticky top-0 z-10">
-              <tr className="bg-[#F9FAFB]">
-                <th className="pl-6 pr-2 py-2 rounded-s-lg">
-                  <div className="flex items-center text-nowrap gap-2 text-[#5D6679] text-sm font-medium w-full cursor-pointer">
-                    <Checkbox
-                      checked={isAllSelected}
-                      indeterminate={isIndeterminate}
-                      onClick={handleSelectAll}
-                    />
-                    <span>Campaign Name</span>
-                    <div
-                      onClick={() => handleSort("name")}
-                      className={` transition-transform duration-300   ${
-                        sortConfig?.key === "name" &&
-                        sortConfig.direction === "asc"
-                          ? "transform rotate-180"
-                          : ""
-                      }`}
-                    >
-                      <ArrowUp color={"#5D6679"} />
-                    </div>
-                  </div>
-                </th>
-                <th className="p-2">
-                  <div className="flex items-center text-nowrap gap-2  text-[#5D6679] text-sm font-medium w-full cursor-pointer">
-                    Organization
-                    <div
-                      onClick={() => handleSort("organization")}
-                      className={` transition-transform duration-300   ${
-                        sortConfig?.key === "organization" &&
-                        sortConfig.direction === "asc"
-                          ? "transform rotate-180"
-                          : ""
-                      }`}
-                    >
-                      <ArrowUp color={"#5D6679"} />
-                    </div>
-                  </div>
-                </th>
-                <th className="p-2">
-                  <div className="flex items-center text-nowrap gap-2  text-[#5D6679] text-sm font-medium w-full cursor-pointer">
-                    Scheduled Date
-                    <div
-                      onClick={() => handleSort("date")}
-                      className={` transition-transform duration-300   ${
-                        sortConfig?.key === "date" &&
-                        sortConfig.direction === "asc"
-                          ? "transform rotate-180"
-                          : ""
-                      }`}
-                    >
-                      <ArrowUp color={"#5D6679"} />
-                    </div>
-                  </div>
-                </th>
-                <th className="p-2">
-                  <div className="flex items-center text-nowrap gap-2  text-[#5D6679] text-sm font-medium w-full cursor-pointer">
-                    Open Rate
-                    <div
-                      onClick={() => handleSort("openRate")}
-                      className={` transition-transform duration-300   ${
-                        sortConfig?.key === "openRate" &&
-                        sortConfig.direction === "asc"
-                          ? "transform rotate-180"
-                          : ""
-                      }`}
-                    >
-                      <ArrowUp color={"#5D6679"} />
-                    </div>
-                  </div>
-                </th>
-                <th className="p-2">
-                  <div className="flex items-center text-nowrap gap-2  text-[#5D6679] text-sm font-medium w-full cursor-pointer">
-                    Opt Out Rate
-                    <div
-                      onClick={() => handleSort("optOutRate")}
-                      className={` transition-transform duration-300   ${
-                        sortConfig?.key === "optOutRate" &&
-                        sortConfig.direction === "asc"
-                          ? "transform rotate-180"
-                          : ""
-                      }`}
-                    >
-                      <ArrowUp color={"#5D6679"} />
-                    </div>
-                  </div>
-                </th>
-
-                <th className="p-2 ">
-                  <div className="flex items-center text-nowrap  text-[#5D6679] text-sm font-medium">
-                    Status
-                  </div>
-                </th>
-                <th className="p-2 rounded-e-lg ">
-                  <div className="flex items-center text-nowrap gap-2  text-[#5D6679] text-sm font-medium w-full cursor-pointer">
-                    Actions
-                  </div>
-                </th>
-              </tr>
-            </thead>
-
-            {campaignData.length !== 0 && (
-              <tbody>
-                {getFilteredCampaigns().map((campaign, index) => (
-                  <tr
-                    key={campaign.id}
-                    className="border-b cursor-pointer border-b-grey-50 hover:bg-gray-50"
-                  >
-                    <td className="text-sm text-nowrap  font-medium flex  gap-2 items-center text-grey-800 p-4 pl-6">
+        <div className="flex flex-col w-full mx-auto mt-[18px] max-h-[814px]">
+          <div className="overflow-x-auto w-full">
+            <table className="w-full">
+              <thead className="text-grey-600 rounded sticky top-0 z-10">
+                <tr className="bg-[#F9FAFB]">
+                  <th className="pl-6 pr-2 py-2 rounded-s-lg">
+                    <div className="flex items-center text-nowrap gap-2 text-[#5D6679] text-sm font-medium w-full cursor-pointer">
                       <Checkbox
-                        checked={selectedItems.includes(index)}
-                        onClick={() => handleSelectItem(index)}
+                        checked={isAllSelected}
+                        indeterminate={isIndeterminate}
+                        onClick={handleSelectAll}
                       />
-                      <div className="w-10 h-10 border border-[#00AAF780]/[0.5] rounded-lg bg-primary-500 flex items-center justify-center">
-                        <SendAlt color="#fff" height={28} width={28} />
-                      </div>
-                      {campaign.name}
-                    </td>
-                    <td className="text-sm font-medium text-grey-800 p-2 pr-8">
-                      {campaign.organization}
-                    </td>
-                    <td className="text-sm font-medium text-grey-800 p-2">
-                      {campaign.date}
-                    </td>
-                    <td className="text-sm font-medium text-grey-800 p-2">
-                      {campaign.openRate}
-                    </td>
-                    <td className="text-sm font-medium text-grey-800 p-2">
-                      {campaign.optOutRate}
-                    </td>
-
-                    <td className="text-sm font-medium  p-2">
+                      <span>Campaign Name</span>
                       <div
-                        className={`p-1 px-2 whitespace-nowrap  ${
-                          campaign.status === "Completed"
-                            ? "bg-success-50 text-success-700"
-                            : campaign.status === "Active"
-                            ? " text-warning-700 bg-warning-50"
-                            : "text-[#344054] bg-[#F2F4F7]"
-                        } rounded-2xl flex items-center gap-2 w-fit`}
+                        onClick={() => handleSort("name")}
+                        className={` transition-transform duration-300   ${
+                          sortConfig?.key === "name" &&
+                          sortConfig.direction === "asc"
+                            ? "transform rotate-180"
+                            : ""
+                        }`}
                       >
-                        <div
-                          className={`rounded-full h-2 w-2 text-success-700 ${
-                            campaign.status === "Completed"
-                              ? " bg-success-500"
-                              : campaign.status === "Active"
-                              ? "bg-[#B54708] "
-                              : "bg-[#667085]"
-                          }`}
-                        ></div>
-                        {campaign.status}
+                        <ArrowUp color={"#5D6679"} />
                       </div>
-                    </td>
-                    <td className="text-sm font-medium gap-2 text-grey-800 p-2 flex items-center">
-                      <Link
-                        href={`/admin/dashboard/usermanagement/campaignience/${campaign.id}`}
+                    </div>
+                  </th>
+                  <th className="p-2">
+                    <div className="flex items-center text-nowrap gap-2  text-[#5D6679] text-sm font-medium w-full cursor-pointer">
+                      Organization
+                      <div
+                        onClick={() => handleSort("organization")}
+                        className={` transition-transform duration-300   ${
+                          sortConfig?.key === "organization" &&
+                          sortConfig.direction === "asc"
+                            ? "transform rotate-180"
+                            : ""
+                        }`}
                       >
-                        {" "}
+                        <ArrowUp color={"#5D6679"} />
+                      </div>
+                    </div>
+                  </th>
+                  <th className="p-2">
+                    <div className="flex items-center text-nowrap gap-2  text-[#5D6679] text-sm font-medium w-full cursor-pointer">
+                      Scheduled Date
+                      <div
+                        onClick={() => handleSort("date")}
+                        className={` transition-transform duration-300   ${
+                          sortConfig?.key === "date" &&
+                          sortConfig.direction === "asc"
+                            ? "transform rotate-180"
+                            : ""
+                        }`}
+                      >
+                        <ArrowUp color={"#5D6679"} />
+                      </div>
+                    </div>
+                  </th>
+                  <th className="p-2">
+                    <div className="flex items-center text-nowrap gap-2  text-[#5D6679] text-sm font-medium w-full cursor-pointer">
+                      Open Rate
+                      <div
+                        onClick={() => handleSort("openRate")}
+                        className={` transition-transform duration-300   ${
+                          sortConfig?.key === "openRate" &&
+                          sortConfig.direction === "asc"
+                            ? "transform rotate-180"
+                            : ""
+                        }`}
+                      >
+                        <ArrowUp color={"#5D6679"} />
+                      </div>
+                    </div>
+                  </th>
+                  <th className="p-2">
+                    <div className="flex items-center text-nowrap gap-2  text-[#5D6679] text-sm font-medium w-full cursor-pointer">
+                      Opt Out Rate
+                      <div
+                        onClick={() => handleSort("optOutRate")}
+                        className={` transition-transform duration-300   ${
+                          sortConfig?.key === "optOutRate" &&
+                          sortConfig.direction === "asc"
+                            ? "transform rotate-180"
+                            : ""
+                        }`}
+                      >
+                        <ArrowUp color={"#5D6679"} />
+                      </div>
+                    </div>
+                  </th>
+
+                  <th className="p-2 ">
+                    <div className="flex items-center text-nowrap  text-[#5D6679] text-sm font-medium">
+                      Status
+                    </div>
+                  </th>
+                  <th className="p-2 rounded-e-lg ">
+                    <div className="flex items-center text-nowrap gap-2  text-[#5D6679] text-sm font-medium w-full cursor-pointer">
+                      Actions
+                    </div>
+                  </th>
+                </tr>
+              </thead>
+
+              {campaignData.length !== 0 && (
+                <tbody>
+                  {getFilteredCampaigns().map((campaign, index) => (
+                    <tr
+                      key={campaign.id}
+                      className="border-b cursor-pointer border-b-grey-50 hover:bg-gray-50"
+                    >
+                      <td className="text-sm text-nowrap  font-medium flex  gap-2 items-center text-grey-800 p-4 pl-6">
+                        <Checkbox
+                          checked={selectedItems.includes(index)}
+                          onClick={() => handleSelectItem(index)}
+                        />
+                        <div className="w-10 h-10 border border-[#00AAF780]/[0.5] rounded-lg bg-primary-500 flex items-center justify-center">
+                          <SendAlt color="#fff" height={28} width={28} />
+                        </div>
+                        {campaign.name}
+                      </td>
+                      <td className="text-sm font-medium text-grey-800 p-2 pr-8">
+                        {campaign.organization}
+                      </td>
+                      <td className="text-sm font-medium text-grey-800 p-2">
+                        {campaign.date}
+                      </td>
+                      <td className="text-sm font-medium text-grey-800 p-2">
+                        {campaign.openRate}
+                      </td>
+                      <td className="text-sm font-medium text-grey-800 p-2">
+                        {campaign.optOutRate}
+                      </td>
+
+                      <td className="text-sm font-medium  p-2">
+                        <div
+                          className={`p-1 px-2 whitespace-nowrap  ${
+                            campaign.status === "Completed"
+                              ? "bg-success-50 text-success-700"
+                              : campaign.status === "Active"
+                              ? " text-warning-700 bg-warning-50"
+                              : "text-[#344054] bg-[#F2F4F7]"
+                          } rounded-2xl flex items-center gap-2 w-fit`}
+                        >
+                          <div
+                            className={`rounded-full h-2 w-2 text-success-700 ${
+                              campaign.status === "Completed"
+                                ? " bg-success-500"
+                                : campaign.status === "Active"
+                                ? "bg-[#B54708] "
+                                : "bg-[#667085]"
+                            }`}
+                          ></div>
+                          {campaign.status}
+                        </div>
+                      </td>
+                      <td className="text-sm font-medium gap-2 text-grey-800 p-2 flex items-center">
+                        <Link
+                          href={`/admin/dashboard/usermanagement/campaignience/${campaign.id}`}
+                        >
+                          {" "}
+                          <Button
+                            size="sm"
+                            icon_style="icon-only"
+                            iconComponent={<Eye color="#858D9D" />}
+                            text="Edit"
+                          />
+                        </Link>
                         <Button
                           size="sm"
+                          iconComponent={<DotV color="#858D9D" />}
                           icon_style="icon-only"
-                          iconComponent={<Eye color="#858D9D" />}
-                          text="Edit"
                         />
-                      </Link>
-                      <Button
-                        size="sm"
-                        iconComponent={<DotV color="#858D9D" />}
-                        icon_style="icon-only"
-                      />
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            )}
-          </table>
-        {mockCampaignData.length >= 10 && (
-          <div className="w-full  pt-[11px] pb-[16px] p-6 ">
-            <Pagination />
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              )}
+            </table>
           </div>
-        )}
-        {mockCampaignData.length === 0 && (
-          <div className="w-full h-80 flex flex-col  text-center  mt-32 mb-32 items-center justify-center mx-auto">
-            <Image src={illustration} alt="img" className="mx-auto" />
-            <p className="text-lg font-semibold">No Content Yet</p>
-            <p className="text-[#475367] text-sm max-w-[260px] w-full mt-1">
-            It looks like no Audience have opted in  through Organizationes.
-            </p>
-          </div>
-        )}
+          {mockCampaignData.length >= 10 && (
+            <div className="w-full  pt-[11px] pb-[16px] p-6 ">
+              <Pagination />
+            </div>
+          )}
+          {mockCampaignData.length === 0 && (
+            <div className="w-full h-80 flex flex-col  text-center  mt-32 mb-32 items-center justify-center mx-auto">
+              <Image src={illustration} alt="img" className="mx-auto" />
+              <p className="text-lg font-semibold">No Content Yet</p>
+              <p className="text-[#475367] text-sm max-w-[260px] w-full mt-1">
+                It looks like no Audience have opted in through Organizationes.
+              </p>
+            </div>
+          )}
         </div>
       </div>
     </div>
