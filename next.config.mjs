0f9201b939a467437c803bf542c7c96e18fc23fd
agbadevemma <agbadevemmanuel@gmail.com>
2025-01-12 21:55:20 +0100

    removed somthing

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    webpack(config, { isServer }) {
-        // Only include the canvas module in the server environment
-        if (isServer) {
-            config.externals = {
-                ...config.externals,
-                canvas: "commonjs canvas",
-            };
-        }
-
-        // Return the updated configuration
-        return config;
-    },
-};
+const nextConfig = {};
 
 export default nextConfig;
