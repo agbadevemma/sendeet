2e3de07b42551100e7929ffd9629a213ef24895e
agbadevemma <agbadevemmanuel@gmail.com>
2025-01-12 21:49:58 +0100

    removed deployment bug

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack(config, { isServer }) {
+        // Only include the canvas module in the server environment
+        if (isServer) {
+            config.externals = {
+                ...config.externals,
+                canvas: "commonjs canvas",
+            };
+        }
+
+        // Return the updated configuration
+        return config;
+    },
+};
 
 export default nextConfig;
